adedbdcf441164d073b6660b50559b8d6c29e904
t.tsujimoto <t.tsujimoto@futurek.com>
2019-07-09 16:06:05 +0900

    Modify : use fileAPI

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,95 +2,131 @@ $(function() {
   /** キャンバス */
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
+  var uploadImgSrc;
 
   /** 書き出す画像のサイズ設定 */
-  var imgWidth = 1960;
-  var imgHeight = 2614;
+  var imgWidth = 1280;
+  var imgHeight = 2274;
+
+  var orientation;
 
   /** ボタン */
   var $retry_btn = $("#retry_btn");
-  var $shot_btn = $("#shot_btn");
-  var $video = $("video");
+  var $footer_block = $("#footer_block");
+  var $upload_btn = $("#upload_btn");
+  var $upload_btn_label = $("#upload_btn_label");
   var $canvas = $("canvas");
-  var $newImg = $("#newImg");
+  var $newImg_block = $("#newImg_block");
 
-  /** カメラの設定 */
-  const medias = {
-    audio: false,
-    video: {
-      facingMode: {
-        exact: "environment" //"user" // リアカメラにアクセス
-      },
-      width: 2614,
-      height: 1960
+  function loadLocalImage(e) {
+    $("body").removeClass("center");
+    setTimeout(function() {
+      $footer_block.show();
+    }, 1000);
+    $upload_btn_label.hide();
+
+    // ファイル情報を取得
+    var fileData = e.target.files[0];
+
+    // 画像ファイル以外は処理を止める
+    if (!fileData.type.match("image.*")) {
+      alert("画像を選択してください");
+      $canvas.hide();
+      $upload_btn_label.show();
+      $footer_block.hide();
+      $newImg_block.hide();
+      $upload_btn.val("");
+      $("body").addClass("center");
+      return;
     }
-  };
-  var video = document.getElementById("video");
-  var promise = navigator.mediaDevices.getUserMedia(medias);
 
-  $video.attr({ width: imgWidth, height: imgHeight });
-  $video.css({
-    width: "100%",
-    height: "calc(100vw * (" + imgHeight + " / " + imgWidth + "))"
-  });
+    EXIF.getData(fileData, function() {
+      orientation = fileData.exifdata.Orientation;
+      console.log(orientation);
+    });
 
-  promise.then(successCallback).catch(errorCallback);
-
-  function successCallback(stream) {
-    video.srcObject = stream;
-  }
-
-  function errorCallback(err) {
-    // alert(err);
-  }
-
-  /** 撮影ボタン */
-  $shot_btn.on("click", function() {
-    try {
-      $video.hide();
-      $retry_btn.show();
-      $shot_btn.hide();
-      video.pause();
-      drawVideo(function() {
+    // FileReaderオブジェクトを使ってファイル読み込み
+    var reader = new FileReader();
+    // ファイル読み込みに成功したときの処理
+    reader.onload = function() {
+      // Canvas上に表示する
+      uploadImgSrc = reader.result;
+      canvasDraw(function() {
         drawDecoration();
       });
-    } catch (e) {
-      alert(e);
-    }
-  });
+    };
+    // ファイル読み込みを実行
+    reader.readAsDataURL(fileData);
+  }
 
-  /** 再撮影ボタン */
-  $retry_btn.on("click", function() {
-    $canvas.hide();
-    $shot_btn.show();
-    $retry_btn.hide();
-    $newImg.hide();
-    $video.show();
-    video.play();
-  });
+  // ファイルが指定された時にloadLocalImage()を実行
+  $upload_btn[0].addEventListener("change", loadLocalImage, false);
 
-  /** 動画の静止画をキャンバスに描画 */
-  function drawVideo(callback) {
+  // Canvas上に画像を表示する
+  function canvasDraw(callback) {
     $canvas.attr({
       width: imgWidth,
       height: imgHeight
     });
     $canvas.css({
-      width: "100%",
-      height: "calc(100vw / 3 * 4)"
+      width: "100%"
+      // height: "calc(100vw / aspect)"
     });
-    context.drawImage(video, 0, 0, imgWidth, imgHeight);
-    callback();
+
+    // canvas内の要素をクリアする
+    context.clearRect(0, 0, imgWidth, imgHeight);
+
+    // Canvas上に画像を表示
+    var img = new Image();
+    img.crossOrigin = "anonymous";
+    img.src = uploadImgSrc;
+    img.onload = function() {
+      var photoWidth = img.width;
+      var photoHeight = img.height;
+      var draw_width = imgHeight;
+      var draw_height = imgWidth;
+      if (orientation == 6) {
+        photoWidth = img.height;
+        photoHeight = img.width;
+        draw_width = photoWidth * (imgWidth / photoWidth);
+        draw_height = photoHeight * (imgWidth / photoWidth);
+      }
+
+      if (orientation == 6) {
+        context.transform(1, 0, 0, 1, imgWidth, 0);
+        context.rotate((90 * Math.PI) / 180);
+      }
+
+      context.drawImage(img, 0, 0, draw_height, draw_width);
+
+      if (orientation == 6) {
+        context.rotate((-90 * Math.PI) / 180);
+        context.transform(1, 0, 0, 1, -imgWidth, 0);
+      }
+
+      callback();
+    };
   }
+
+  /** 再撮影ボタン */
+  $retry_btn.on("click", function() {
+    $canvas.hide();
+    $upload_btn_label.show();
+    $footer_block.hide();
+    $newImg_block.hide();
+    $upload_btn.val("");
+    $("body").addClass("center");
+  });
 
   /** 飾り用画像を上書き */
   function drawDecoration() {
     var img = new Image();
     img.crossOrigin = "anonymous";
-    img.src = "./img/overimg.png";
+    img.src = "./img/deco.png";
 
     img.onload = function() {
-      context.drawImage(img, imgWidth / 2 - 749 / 2, 30); // 749 x 289
+      // context.drawImage(img, imgWidth / 2 - 749 / 2, 30); // 749 x 289
+      context.drawImage(img, 0, 0, imgWidth, imgHeight);
       // context.drawImage(img, 30, 30); // 749 x 289
 
       // var dstWidth = this.width;
@@ -116,7 +152,7 @@ $(function() {
     var png = canvas.toDataURL("image/png");
     document.getElementById("newImg").src = png;
 
-    $newImg.show();
+    $newImg_block.show();
     $canvas.hide();
   }
 });
